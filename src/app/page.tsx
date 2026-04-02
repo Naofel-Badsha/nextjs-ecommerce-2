77566d87a0d88fcf6d18c09b1ec738bf5f1e41e2
Naofel Badsha <naofelbadsha@gmail.com>
2026-04-03 02:08:54 +0600

    addedd shadcen

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
     <div className="">
       <h1 className="">WelCome ByuNest App</h1>
+      <Button>Hello</Button>
     </div>
   );
 }
