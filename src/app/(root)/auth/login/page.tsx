37546ba32430eed2708f88ffb2a2518fcd2d256c
Naofel Badsha <naofelbadsha@gmail.com>
2026-04-05 02:08:48 +0600

    added font family

--- src/app/(root)/auth/login/page.tsx
+++ src/app/(root)/auth/login/page.tsx
@@ -1,14 +1,22 @@
-import { Card, CardContent } from '@/components/ui/card'
-import React from 'react'
+import { Card, CardContent } from "@/components/ui/card";
+import Image from "next/image";
 
 const LoginPage = () => {
   return (
     <section>
-        <Card>
-            <CardContent></CardContent>
+        <Card className="w-[450px]">
+            <CardContent>
+                <div className="flex items-center justify-center">
+                    <Image src="https://i.ibb.co.com/27jzrDyw/logo.png" alt='Logo' width={100} height={100}/>
+                </div>
+                <div className="space-y-2 text-center">
+                    <h1 className="text-xl font-semibold mt-5">Login Into Account</h1>
+                    <p className="text-[14px]">Login into your account by filling out the form below.</p>
+                </div>
+            </CardContent>
         </Card>
     </section>
   )
 }
 
-export default LoginPage
+export default LoginPage;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,12 @@
 import { Button } from '@/components/ui/button'
+import Image from 'next/image'
 import React from 'react'
 
 const Home = () => {
   return (
     <div>
       <Button>Hello</Button>
-      {/* 
-      
-      DbUser: buynext-ecommerce
-      Pass: buynext-ecommerce
-      */}
+<Image src="https://i.ibb.co.com/N2WqZ30r/imageye-imgi-6-product3.webp" alt='Logo' width={100} height={100}/>
     </div>
   )
 }
