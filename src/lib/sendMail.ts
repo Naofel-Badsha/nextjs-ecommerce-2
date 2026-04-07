30590ddf7a5e190802c75788bcd58f42f178d8f8
Naofel Badsha <naofelbadsha@gmail.com>
2026-04-08 01:25:08 +0600

    added sendMailer

---
+++ src/lib/sendMail.ts
@@ -0,0 +1,30 @@
+import nodemailer from "nodemailer";
+
+export const sendMail = async (
+  subject: string,
+  receiver: string,
+  body: string,
+) => {
+  const transporter = nodemailer.createTransporter({
+    host: process.env.NODEMAILER_HOST,
+    port: process.env.NODEMAILER_PORT,
+    secure: false,
+    auth: {
+      user: process.env.NODEMAILER_EMAIL,
+      pass: process.env.NODEMAILER_PASS,
+    },
+  });
+
+  const options = {
+    from: `Developer Badshah <${process.env.NODEMAILER_EMAIL}>`,
+    to: receiver,
+    subject: subject,
+    html: body,
+  };
+  try {
+    await transporter.sendMail(options);
+    return { success: true, message: "Email sent successfully" };
+  } catch (error) {
+    return { success: false, message: "Failed to send email" };
+  }
+};
