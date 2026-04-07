de7479b97da5ef354489f33ad9bbee094b9b94f7
Naofel Badsha <naofelbadsha@gmail.com>
2026-04-08 02:17:19 +0600

    added email veryfivation link

--- src/app/api/auth/register/route.ts
+++ src/app/api/auth/register/route.ts
@@ -1,49 +1,61 @@
 import { connectDB } from "@/lib/databaseConnection";
 import { response } from "@/lib/helperFunction";
 import { zodSchema } from "@/lib/zodSchema";
-import { SignJWT} from "jose";
+import { SignJWT } from "jose";
 import UserModal from "../../../../../models/User.model";
+import { sendMail } from "@/lib/sendMail";
+import { emailVerificationLink } from "../../../../../email/emailVerificationLink";
 
 export async function POST(request: Request) {
-    try {
-        await connectDB();
-        const validationScema = zodSchema.pick({ 
-            email: true, password: true, username: true 
-        });
-        const paylod = await request.json();
-        const validateData = validationScema.safeParse(paylod);
-        if (!validateData.success) {
-            return response(false, 401, "Invalid or missing input fields", 
-               validateData.error);
-        }
-        const { email, password, username } = validateData.data;
-
-        //--------Cheak already registered user-------
-        const cheakUser = await UserModal.exists({ email });
-        if (cheakUser) {
-            return response(true, 409, "User already registered");
-        }
-
-
-        //---------new resgidtion user----------
-        const NewRegistation = new UserModal({
-            email,
-            password,   
-            username
-        });
-        await NewRegistation.save();
-        const secret = new TextEncoder().encode(process.env.JWT_SECRET_KEY!);
-        const token = await new SignJWT({userId: NewRegistation._id})
-        .setIssuedAt()
-        .setExpirationTime("2h")
-        .setProtectedHeader({ alg: "HS256"})
-        .sign(secret);   
-
-        return response(true, 201, "User registered successfully", { user: NewRegistation });
-
-
-    } catch (error) {
-        console.error("Error connecting to database:", error);
-        return response(false, 500, "Internal Server Error");
+  try {
+    await connectDB();
+    const validationScema = zodSchema.pick({
+      email: true,
+      password: true,
+      username: true,
+    });
+    const paylod = await request.json();
+    const validateData = validationScema.safeParse(paylod);
+    if (!validateData.success) {
+      return response(
+        false,
+        401,
+        "Invalid or missing input fields",
+        validateData.error,
+      );
     }
+    const { email, password, username } = validateData.data;
+
+    //--------Cheak already registered user-------
+    const cheakUser = await UserModal.exists({ email });
+    if (cheakUser) {
+      return response(true, 409, "User already registered");
+    }
+
+    //---------new resgidtion user----------
+    const NewRegistation = new UserModal({
+      email,
+      password,
+      username,
+    });
+    await NewRegistation.save();
+    const secret = new TextEncoder().encode(process.env.JWT_SECRET_KEY!);
+    const token = await new SignJWT({ userId: NewRegistation._id })
+      .setIssuedAt()
+      .setExpirationTime("2h")
+      .setProtectedHeader({ alg: "HS256" })
+      .sign(secret);
+
+    await sendMail(
+      "Email verification request form Developer Badshah",
+      email,
+      emailVerificationLink(
+        `${process.env.NEXT_PUBLIC_BASE_URL}/api/auth/verify-email?token=${token}`,
+      ),
+    );
+    return response(true, 200, "User registered successfully, please verify your email");
+  } catch (error) {
+    console.error("Error connecting to database:", error);
+    return response(false, 500, "Internal Server Error");
+  }
 }
--- src/lib/helperFunction.ts
+++ src/lib/helperFunction.ts
@@ -4,3 +4,27 @@ import { NextResponse } from "next/server";
 export const response = (success: boolean, statusCode: number, message: string, data: any = {}) => {
     return NextResponse.json({ success, statusCode, message, data });
 };
+
+export const catchError = (error: any, customMessage: string ) => {
+    //-------Handaling duplicate key erroe-------
+    if (error.code === 11000) {
+        const key = Object.keys(error.keyPattern).join(", ");
+        error.message = `Duplicate key error: ${key} Those fields must be unique.`;
+    } else {
+        console.error("Error:", error);
+    }
+
+    let errorObj = {}
+
+    if(process.env.NODE_ENV === "development"){
+        errorObj = { 
+            message: error.message, 
+            error
+        }
+    }else{
+        errorObj = { 
+            message: customMessage || 'Internal Server Error', 
+        }
+    }
+    return response(false, 500, customMessage, errorObj);
+}
