ec1955d30d6db170b473946ed937eba0e74facc4
Naofel-Badsha <naofelbadsha@gmail.com>
2026-04-11 19:31:25 +0600

    added siderbaer logo

--- src/components/ApplicationUi/Admin/AppSidebar.tsx
+++ src/components/ApplicationUi/Admin/AppSidebar.tsx
@@ -1,9 +1,40 @@
-import React from 'react'
+import React from "react";
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarGroup,
+  SidebarHeader,
+} from "@/components/ui/sidebar";
+import Image from "next/image";
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
+import { FaBars } from "react-icons/fa";
 
 const AppSidebar = () => {
   return (
-    <div>AppSidebar</div>
-  )
-}
+    <Sidebar>
+      <SidebarHeader className="border-b h-14 p-0 px-1">
+        <div className="flex items-center justify-between gap-2 p-2">
+          <Link href="/" className="flex items-center gap-2">
+            <Image
+              src="https://i.ibb.co.com/27jzrDyw/logo.png"
+              alt="Logo"
+              width={32}
+              height={32}
+            />
+            <span className="font-medium">BuyNest</span>
+          </Link>
+          <Button className="cursor-pointer"><FaBars /></Button>
+        </div>
+      </SidebarHeader>
+      <SidebarContent>
+        <SidebarGroup />
+        <SidebarGroup />
+      </SidebarContent>
+      <SidebarFooter />
+    </Sidebar>
+  );
+};
 
-export default AppSidebar
+export default AppSidebar;
